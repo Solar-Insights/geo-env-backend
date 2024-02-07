033030ae5a1a0d09c2bb20913aa810972afd3889
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-07 12:20:37 -0500

    Mathis feature: modular routers, index.ts will use them

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+// Routers
+import solarRouter from "@/routes/solar";
+import airQualityRouter from "@/routes/airQuality";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
+app.use(airQualityRouter);
+app.use(solarRouter);
+
 app.listen(port, () => {
     console.log("test");
 });
