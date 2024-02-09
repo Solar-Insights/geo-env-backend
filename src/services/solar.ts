c9fc4f4082f275c90625da7b0189e1300d3d1ddd
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-09 14:02:14 -0500

    Mathis doc/refactor: Doc for closestBuildingInsights route & removed unused import

--- src/services/solar.ts
+++ src/services/solar.ts
@@ -1,16 +1,17 @@
 import axios from "axios"
 import { GOOGLE_KEY } from "@/config";
-import { Coordinates, validCoordinates } from "solar-typing/src/general"
+import { Coordinates } from "solar-typing/src/general"
 import { BuildingInsights } from "solar-typing/src/solar"
 import { Client, } from "@googlemaps/google-maps-services-js"
 
 const client = new Client({});
 
 export async function getClosestBuildingInsights(coord: Coordinates) {
+    // https://developers.google.com/maps/documentation/solar/reference/rest/v1/buildingInsights/findClosest
     return await axios({
         method: "get",
         responseType: 'json',
-        url: `https://solar.googleapis.com/v1/buildingInsights:findClosest`,
+        url: "https://solar.googleapis.com/v1/buildingInsights:findClosest",
         params: {
             key: GOOGLE_KEY,
             "location.latitude": coord.lat.toFixed(5),
