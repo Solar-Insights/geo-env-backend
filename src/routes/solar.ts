33626c392d7ae53563e1ba40d118494858a26b46
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-09 14:24:03 -0500

    Mathis feat: getSolarLayers and getSingleSolarLayer requests

--- src/routes/solar.ts
+++ src/routes/solar.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import { generalErrorResponse } from "@/errorHandling/errorResponse";
 import { Coordinates } from "solar-typing/src/general"
-import {} from "solar-typing/src/solar"
-import { getClosestBuildingInsights } from "@/services/solar"
+import { BuildingInsights, SolarLayers } from "solar-typing/src/solar"
+import { getClosestBuildingInsights, getSolarLayers } from "@/services/solar"
 
 const solarRouter = express.Router();
 
@@ -12,10 +12,29 @@ solarRouter.get("/solar/closest-building-insights", async (req, res) => {
         lng: Number(req.query.lng)
     };
 
-    const buildingInsights = await getClosestBuildingInsights(coord); 
+    const buildingInsights: BuildingInsights | null = await getClosestBuildingInsights(coord); 
     if (buildingInsights !== null) {
         res.status(200).json({
             buildingInsights: buildingInsights
+        });
+    } else {
+        res.status(300).json(
+            generalErrorResponse("something wrong happened")
+        );
+    }
+});
+
+solarRouter.get("/solar/solar-layers", async (req, res) => {
+    const radius: number = Number(req.query.radius);
+    const coord: Coordinates = {
+        lat: Number(req.query.lat),
+        lng: Number(req.query.lng)
+    };
+
+    const solarLayers: SolarLayers | null = await getSolarLayers(coord, radius); 
+    if (solarLayers !== null) {
+        res.status(200).json({
+            solarLayers: solarLayers
         });
     } else {
         res.status(300).json(
--- src/services/solar.ts
+++ src/services/solar.ts
@@ -1,8 +1,11 @@
 import axios from "axios"
 import { GOOGLE_KEY } from "@/config";
 import { Coordinates } from "solar-typing/src/general"
-import { BuildingInsights } from "solar-typing/src/solar"
+import { BuildingInsights, LayerId, Layer, SolarLayers } from "solar-typing/src/solar"
+
 import { Client, } from "@googlemaps/google-maps-services-js"
+import { downloadGeoTIFF, renderPalette } from "@/misc/solar";
+import { ironPalette, sunlightPalette} from "@/misc/constants";
 
 const client = new Client({});
 
@@ -25,4 +28,115 @@ export async function getClosestBuildingInsights(coord: Coordinates) {
             console.log(error);
             return null;
         });
+}
+
+export async function getSolarLayers(coord: Coordinates, radius: number) {
+    // https://developers.google.com/maps/documentation/solar/data-layers
+    return await axios({
+        method: "get",
+        responseType: 'json',
+        url: "https://solar.googleapis.com/v1/dataLayers:get",
+        params: {
+            key: GOOGLE_KEY,
+            "location.latitude": coord.lat.toFixed(5),
+            "location.longitude": coord.lng.toFixed(5),
+            radiusMeters: radius.toString(),
+            view: "FULL_LAYERS",
+            requiredQuality: "HIGH",
+        }
+    })
+        .then((response) => {
+            return response.data as SolarLayers;
+        })
+        .catch((error) => {
+            console.log(error);
+            return null;
+        });
+}
+
+export async function getSingleLayer(layerId: LayerId, urls: SolarLayers) {
+    const get: Record<LayerId, () => Promise<Layer>> = {
+        annualFlux: async () => {
+            const [mask, data] = await Promise.all([
+                downloadGeoTIFF(urls.maskUrl),
+                downloadGeoTIFF(urls.annualFluxUrl),
+            ]);
+            const colors = ironPalette;
+            return {
+                id: layerId,
+                bounds: mask.bounds,
+                palette: {
+                    colors: colors,
+                    min: "Shady",
+                    max: "Sunny",
+                },
+                render: (showRoofOnly) => [
+                    renderPalette({
+                        data: data,
+                        mask: showRoofOnly ? mask : undefined,
+                        colors: colors,
+                        min: 0,
+                        max: 1800,
+                    }),
+                ],
+            };
+        },
+        monthlyFlux: async () => {
+            const [mask, data] = await Promise.all([
+                downloadGeoTIFF(urls.maskUrl),
+                downloadGeoTIFF(urls.monthlyFluxUrl),
+            ]);
+            const colors = ironPalette;
+            return {
+                id: layerId,
+                bounds: mask.bounds,
+                palette: {
+                    colors: colors,
+                    min: "Shady",
+                    max: "Sunny",
+                },
+                render: (showRoofOnly) =>
+                    [...Array(12).keys()].map((month) =>
+                        renderPalette({
+                            data: data,
+                            mask: showRoofOnly ? mask : undefined,
+                            colors: colors,
+                            min: 0,
+                            max: 200,
+                            index: month,
+                        }),
+                    ),
+            };
+        },
+        hourlyShade: async () => {
+            const [mask, ...months] = await Promise.all([
+                downloadGeoTIFF(urls.maskUrl),
+                ...urls.hourlyShadeUrls.map((url) => downloadGeoTIFF(url)),
+            ]);
+            const colors = sunlightPalette;
+            return {
+                id: layerId,
+                bounds: mask.bounds,
+                palette: {
+                    colors: colors,
+                    min: "Shade",
+                    max: "Sun",
+                },
+                render: (showRoofOnly, month, day) =>
+                    [...Array(24).keys()].map((hour) =>
+                        renderPalette({
+                            data: {
+                                ...months[month],
+                                rasters: months[month].rasters.map((values) => values.map((x) => x & (1 << (day - 1)))),
+                            },
+                            mask: showRoofOnly ? mask : undefined,
+                            colors: colors,
+                            min: 0,
+                            max: 1,
+                            index: hour,
+                        }),
+                    ),
+            };
+        },
+    };
 }
