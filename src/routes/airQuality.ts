4e7dc1e2c897a448493b6e2ad9d966337957ba44
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-08 20:56:22 -0500

    Mathis feat: default route and import placeholder for air/solar

--- src/routes/airQuality.ts
+++ src/routes/airQuality.ts
@@ -1,5 +1,15 @@
 import express from "express";
+import {} from "solar-typing/src/general"
+import {} from "solar-typing/src/solar"
 
 const router = express.Router();
+
+
+router.get("/", (req, res) => {
+    console.log(1234);
+    res.json({
+        test: "test"
+    });
+});
 
 export default router;
--- src/routes/solar.ts
+++ src/routes/solar.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import {} from "solar-typing/src/general"
+import {} from "solar-typing/src/solar"
 
 const router = express.Router();
+
 
 router.get("/", (req, res) => {
     console.log(1234);
