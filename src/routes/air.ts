4a9c5fa6d2577d5c2d83bdf4ebae710458decf53
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-09 14:01:42 -0500

    Mathis feat: getAirQualityData route

--- src/routes/air.ts
+++ src/routes/air.ts
@@ -1,6 +1,8 @@
 import express from "express";
-import {} from "solar-typing/src/general"
-import {} from "solar-typing/src/solar"
+import { generalErrorResponse } from "@/errorHandling/errorResponse";
+import { Coordinates } from "solar-typing/src/general"
+import {} from "solar-typing/src/airQuality"
+import { getAirQualityData } from "@/services/air";
 
 const airRouter = express.Router();
 
@@ -10,6 +12,24 @@ airRouter.get("/", (req, res) => {
     res.json({
         test: "test"
     });
+});
+
+airRouter.get("/air/air-quality-data", async (req, res) => {
+    const coord: Coordinates = {
+        lat: Number(req.query.lat),
+        lng: Number(req.query.lng)
+    };
+
+    const airQualityData = await getAirQualityData(coord); 
+    if (airQualityData !== null) {
+        res.status(200).json({
+            airQualityData: airQualityData
+        });
+    } else {
+        res.status(300).json(
+            generalErrorResponse("something wrong happened")
+        );
+    }
 });
 
 export default airRouter;
---
+++ src/services/air.ts
@@ -0,0 +1,39 @@
+import axios from "axios"
+import { GOOGLE_KEY } from "@/config";
+import { Coordinates } from "solar-typing/src/general"
+import { AirQualityData } from "solar-typing/src/airQuality"
+import { Client, } from "@googlemaps/google-maps-services-js"
+
+const client = new Client({});
+
+export async function getAirQualityData(coord: Coordinates) {
+    // https://developers.google.com/maps/documentation/air-quality/reference/rest/v1/currentConditions/lookup#request-body
+    return await axios({
+        method: "post",
+        responseType: 'json',
+        url: "https://airquality.googleapis.com/v1/currentConditions:lookup",
+        params: {
+            key: GOOGLE_KEY,
+        },
+        data: {
+            location: {
+                latitude: coord.lat,
+                longitude: coord.lng
+            },
+            universalAqi: true,
+            extraComputations: [
+                "DOMINANT_POLLUTANT_CONCENTRATION",
+                "POLLUTANT_CONCENTRATION",
+                "POLLUTANT_ADDITIONAL_INFO",
+                "HEALTH_RECOMMENDATIONS",
+            ],
+        }
+    })
+        .then((response) => {
+            return response.data as AirQualityData;
+        })
+        .catch((error) => {
+            console.log(error);
+            return null;
+        });
+}
