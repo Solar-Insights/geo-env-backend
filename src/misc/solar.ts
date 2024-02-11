4627cf4a7d087e05619cfb4b8e24db178b6de55d
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-11 13:36:55 -0500

    Mathis refactor: relayed the geotiff making job to misc functions

--- src/misc/solar.ts
+++ src/misc/solar.ts
@@ -1,4 +1,7 @@
 import { GeoTiff } from "solar-typing/src/solar";
+import * as geotiff from "geotiff";
+import geokeysToProj4 from "geotiff-geokeys-to-proj4";
+import proj4 from "proj4";
 
 export function renderRGB(rgb: GeoTiff, mask?: GeoTiff) {
     // https://www.w3schools.com/tags/canvas_createimagedata.asp
@@ -106,4 +109,42 @@ export function normalize(x: number, max: number = 1, min: number = 0) {
 
 export function clamp(x: number, min: number, max: number) {
     return Math.min(Math.max(x, min), max);
+}
+
+export async function makeGeotiff(data: any) {
+    const buffer: Buffer = data.data;
+    const arraybuffer: ArrayBuffer = buffer.buffer.slice(
+        buffer.byteOffset,
+        buffer.byteOffset + buffer.byteLength,
+    );
+
+    const tiff = await geotiff.fromArrayBuffer(arraybuffer);
+    const image = await tiff.getImage();
+    const rasters = await image.readRasters();
+
+    const geoKeys = image.getGeoKeys();
+
+    const projObj = geokeysToProj4.toProj4(geoKeys);
+    const projection = proj4(projObj.proj4, "WGS84");
+    const box = image.getBoundingBox();
+    const sw = projection.forward({
+        x: box[0] * projObj.coordinatesConversionParameters.x,
+        y: box[1] * projObj.coordinatesConversionParameters.y,
+    });
+    const ne = projection.forward({
+        x: box[2] * projObj.coordinatesConversionParameters.x,
+        y: box[3] * projObj.coordinatesConversionParameters.y,
+    });
+
+    return {
+        width: rasters.width,
+        height: rasters.height,
+        rasters: [...Array(rasters.length).keys()].map((i) => Array.from(rasters[i] as geotiff.TypedArray)),
+        bounds: {
+            north: ne.y,
+            south: sw.y,
+            east: ne.x,
+            west: sw.x,
+        },
+    } as GeoTiff;
 }
--- src/services/solar.ts
+++ src/services/solar.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 import { GOOGLE_KEY } from "@/config";
 import { Coordinates } from "solar-typing/src/general";
-import { BuildingInsights, SolarLayers, GeoTiff } from "solar-typing/src/solar";
+import { BuildingInsights, SolarLayers } from "solar-typing/src/solar";
 import { Client } from "@googlemaps/google-maps-services-js";
-import * as geotiff from "geotiff";
-import geokeysToProj4 from "geotiff-geokeys-to-proj4";
-import proj4 from "proj4";
+import { makeGeotiff } from "@/misc/solar";
 
 const client = new Client({});
 
@@ -62,41 +60,7 @@ export async function getGeotiff(url: string) {
         },
     })
         .then(async (response) => {
-            const buffer: Buffer = response.data;
-            const arraybuffer: ArrayBuffer = buffer.buffer.slice(
-                buffer.byteOffset,
-                buffer.byteOffset + buffer.byteLength,
-            );
-
-            const tiff = await geotiff.fromArrayBuffer(arraybuffer);
-            const image = await tiff.getImage();
-            const rasters = await image.readRasters();
-
-            const geoKeys = image.getGeoKeys();
-
-            const projObj = geokeysToProj4.toProj4(geoKeys);
-            const projection = proj4(projObj.proj4, "WGS84");
-            const box = image.getBoundingBox();
-            const sw = projection.forward({
-                x: box[0] * projObj.coordinatesConversionParameters.x,
-                y: box[1] * projObj.coordinatesConversionParameters.y,
-            });
-            const ne = projection.forward({
-                x: box[2] * projObj.coordinatesConversionParameters.x,
-                y: box[3] * projObj.coordinatesConversionParameters.y,
-            });
-
-            return {
-                width: rasters.width,
-                height: rasters.height,
-                rasters: [...Array(rasters.length).keys()].map((i) => Array.from(rasters[i] as geotiff.TypedArray)),
-                bounds: {
-                    north: ne.y,
-                    south: sw.y,
-                    east: ne.x,
-                    west: sw.x,
-                },
-            } as GeoTiff;
+            return await makeGeotiff(response);
         })
         .catch((error) => {
             throw error;
