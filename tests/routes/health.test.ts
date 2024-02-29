0e970b0f2984ef770378b7d6ee3137d295e6749d
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-02-28 22:33:20 -0500

    Mathis tests: simple test for health route

---
+++ tests/routes/health.test.ts
@@ -0,0 +1,19 @@
+import { describe, test, assert } from "vitest";
+import request from "supertest";
+import { ServerFactory } from "@/serverFactory";
+import { ApiError } from "@/misc/customErrors";
+
+const { app } = ServerFactory.create().onTestEnvironnement().withDefaultValues().build();
+
+describe(`GET /health`, async () => {
+    const url = `/health`;
+
+    test("whenRequestIsSucessfull, then returns 200 with server is up message", () => {
+        return request(app)
+            .get(url)
+            .expect(200)
+            .then((response) => {
+                assert.isTrue(response.body.status === "server is up");
+            });
+    });
+});
