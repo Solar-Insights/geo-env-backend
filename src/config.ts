48fb3198c9554a8a5e6b3c82d2d277fedc40b0ad
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-09 20:21:02 -0400

    Mathis chore: env var for backend auth with auth0

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,15 @@
 import dotenv from "dotenv";
+import { auth } from "express-oauth2-jwt-bearer";
 
 dotenv.config();
 
+export const PORT: number = Number(process.env.PORT) || 8080;
 export const GOOGLE_KEY: string = process.env.GOOGLE_KEY as string;
-export const PORT: number = Number(process.env.PORT);
+export const BACKEND_URL: string = process.env.BACKEND_URL as string;
+export const AUTH0_BASE_URL: string = process.env.AUTH0_BASE_URL as string;
+
+export const jwtCheck = auth({
+    audience: BACKEND_URL,
+    issuerBaseURL: AUTH0_BASE_URL,
+    tokenSigningAlg: 'RS256'
+});;
