581e13a89502c942eb4f5295871d15066e9dc096
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 20:57:43 -0400

    Mathis feat: Health route corrected

--- tests/routes/health.test.ts
+++ tests/routes/health.test.ts
@@ -1,15 +1,18 @@
 import { describe, test, assert } from "vitest";
 import request from "supertest";
-import { ServerFactory } from "@/serverFactory";
+import { serverFactory } from "./factory";
+import { getAuthTokenForTest } from "../auth/auth";
 
-const { app } = ServerFactory.create().onTestEnvironnement().withDefaultValues().build();
+const app = serverFactory.app;
+const token = await getAuthTokenForTest();
 
 describe(`GET /health`, async () => {
     const url = `/health`;
 
-    test("whenRequestIsSucessfull, then returns 200 with server is up message", () => {
+    test("when request is sucessfull, then returns 200 with server is up message", async () => {
         return request(app)
             .get(url)
+            .set('Authorization', `Bearer ${token}`)
             .expect(200)
             .then((response) => {
                 assert.isTrue(response.body.status === "server is up");
