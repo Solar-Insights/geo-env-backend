d89446d2196262b0d62f88373d13c67d8f110f09
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 20:56:57 -0400

    Mathis feat: Using port 0 if not specified: lets OS choose for us.

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT: number = Number(process.env.PORT) || 8080;
+export const PORT: number = Number(process.env.PORT) || 0;
 export const GOOGLE_KEY: string = process.env.GOOGLE_KEY as string;
 export const BACKEND_URL: string = process.env.BACKEND_URL as string;
 export const AUTH0_BASE_URL: string = process.env.AUTH0_BASE_URL as string;
