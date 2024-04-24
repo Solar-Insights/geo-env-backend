48524d1222df81065544b58944e679158966d523
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 20:48:00 -0400

    Mathis feat: Getting auth token for tests

---
+++ tests/auth/auth.ts
@@ -0,0 +1,30 @@
+import { BACKEND_URL, AUTH0_TESTING_CLIENT_ID, AUTH0_TESTING_CLIENT_SECRET } from "@/config";
+import axios from "axios";
+
+let token: string | undefined;
+
+export async function getAuthTokenForTest() {
+    if (token) return token;
+
+    return await axios({
+        method: "post",
+        url: "https://dev-ubs32bgn56n1z15q.us.auth0.com/oauth/token",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        data: {
+            "client_id": AUTH0_TESTING_CLIENT_ID,
+            "client_secret": AUTH0_TESTING_CLIENT_SECRET,
+            "audience": BACKEND_URL,
+            "grant_type": "client_credentials"
+        },
+    })
+        .then(async (response) => {
+            console.log("Successfully acquired Auth Token for testing");
+            token = response.data.access_token;
+            return token;
+        })
+        .catch((error) => {
+            console.log("Can't get Auth Token for testing");
+        });
+}
