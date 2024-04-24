6a80520702963cfb22e238feb755cd3cbbb1d56b
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 21:59:25 -0400

    Mathis feat: Corrected and improved all solar tests

--- tests/routes/solar.test.ts
+++ tests/routes/solar.test.ts
@@ -1,96 +1,140 @@
 import { describe, test, assert, vi } from "vitest";
 import request from "supertest";
 import nock from "nock";
-import { ServerFactory } from "@/serverFactory";
 import { GOOGLE_KEY } from "@/config";
 import { dummyBuildingInsights, dummyGeoTiff, dummySolarLayers } from "geo-env-typing/solar";
-import { dummyLatLng } from "geo-env-typing/geo";
+import { dummyLatLng, LatLng } from "geo-env-typing/geo";
 import { UtilGenerator, NumberGenerator, StringGenerator } from "geo-env-typing/generators";
 import { ApiError } from "@/middlewares/customErrors";
+import { serverFactory } from "./factory";
+import { getAuthTokenForTest } from "../auth/auth";
+import { makeInvalidCoordError } from "@/middlewares/requestHandlers";
 
-const { app } = ServerFactory.create().onTestEnvironnement().withDefaultValues().build();
-const solar = await import("@/services/solar");
-vi.mock("@/misc/solar");
+const app = serverFactory.app;
+const token = await getAuthTokenForTest();
 
 const GoogleSolarApiUrl = "https://solar.googleapis.com/v1/";
+const solar = await import("@/services/solar");
+vi.mock("@/services/solar");
 
-describe(`GET /solar/closest-building-insights`, async () => {
-    const dummyData = dummyBuildingInsights();
-    const dummyCoord = dummyLatLng();
-    const url = `/solar/closest-building-insights?${new URLSearchParams(dummyCoord as any).toString()}`;
 
-    const nockInstance = nock(GoogleSolarApiUrl)
+function getClosestBuildingInsightsEndpoint(coord: LatLng) {
+    return `/solar/closest-building-insights?${new URLSearchParams(coord as any).toString()}`;
+}
+
+function getBuildingInsightsNockInstance(coord: LatLng) {
+    return nock(GoogleSolarApiUrl)
         .get("/buildingInsights:findClosest")
         .query({
             key: GOOGLE_KEY,
-            "location.latitude": dummyCoord.lat.toFixed(5),
-            "location.longitude": dummyCoord.lng.toFixed(5)
+            "location.latitude": coord.lat.toFixed(5),
+            "location.longitude": coord.lng.toFixed(5)
         });
+}
 
-    test("whenRequestIsSucessfull, then returns 200 with building insights", () => {
+describe(`GET /solar/closest-building-insights`, async () => {
+    test("given valid coordinates, when request is successfull, then returns 200 with building insights", async () => {
+        const dummyData = dummyBuildingInsights();
+        const dummyCoord = dummyLatLng();
+        const endpoint = getClosestBuildingInsightsEndpoint(dummyCoord);
+
+        const nockInstance = getBuildingInsightsNockInstance(dummyCoord)
         nockInstance.reply(200, dummyData);
 
-        return request(app)
-            .get(url)
+        return await request(app)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
             .expect(200)
             .then((response) => {
                 assert.isTrue(UtilGenerator.identicalJsonStrings(response.body.buildingInsights, dummyData));
             });
     });
 
-    test("whenRequestFails, then returns 500 with api error", () => {
-        const apiError = new ApiError(url);
+    test("given error on api call, when request fails, then returns 500 with api error", async () => {
+        const dummyCoord = dummyLatLng();
+        const endpoint = getClosestBuildingInsightsEndpoint(dummyCoord);
 
+        const apiError = new ApiError(endpoint);
+        const nockInstance = getBuildingInsightsNockInstance(dummyCoord)
         nockInstance.replyWithError(apiError);
 
         return request(app)
-            .get(url)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
             .expect(500)
             .then((response) => {
                 assert.isTrue(UtilGenerator.identicalJsonStrings(response.body, apiError.toObject()));
             });
     });
+
+    test("given invalid coordinates, when request fails, then returns 400 with invalid parameter error", async () => {
+        const dummyData = dummyBuildingInsights();
+        const dummyCoord = { lat: 1000, lng: 1000 };
+        const endpoint = getClosestBuildingInsightsEndpoint(dummyCoord);
+
+        const nockInstance = getBuildingInsightsNockInstance(dummyCoord)
+        nockInstance.reply(200, dummyData);
+
+        return await request(app)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
+            .expect(400)
+            .then((response) => {
+                assert.isTrue(UtilGenerator.identicalJsonStrings(response.body, makeInvalidCoordError(endpoint).toObject()));
+            });
+    });
 });
 
-describe(`GET /solar/solar-layers`, async () => {
-    const dummyData = dummySolarLayers();
-    const dummyCoord = dummyLatLng();
-    const radius = NumberGenerator.generateDouble(100);
-    const params = {
-        lat: dummyCoord.lat,
-        lng: dummyCoord.lng,
-        radius: radius
-    };
-    const url = `/solar/solar-layers?${new URLSearchParams(params as any).toString()}`;
-    const apiError = new ApiError(url);
 
-    const nockInstance = nock(GoogleSolarApiUrl)
+function getSolarLayersEndpoint(params: any) {
+    return `/solar/solar-layers?${new URLSearchParams(params as any).toString()}`;
+}
+
+function getSolarLayersNockInstance(coord: LatLng, radius: number) {
+    return nock(GoogleSolarApiUrl)
         .get("/dataLayers:get")
         .query({
             key: GOOGLE_KEY,
-            "location.latitude": dummyCoord.lat.toFixed(5),
-            "location.longitude": dummyCoord.lng.toFixed(5),
+            "location.latitude": coord.lat.toFixed(5),
+            "location.longitude": coord.lng.toFixed(5),
             radiusMeters: radius.toString(),
             view: "FULL_LAYERS",
             requiredQuality: "HIGH"
         });
+}
 
-    test("whenRequestIsSucessfull, then returns 200 with solar layers", () => {
+describe(`GET /solar/solar-layers`, async () => {
+    const radius = NumberGenerator.generateDouble(100);
+
+    test("given valid coordinates, when request is successfull, then returns 200 with solar layers", () => {
+        const dummyData = dummySolarLayers();
+        const dummyCoord = dummyLatLng();
+        const endpoint = getSolarLayersEndpoint({ lat: dummyCoord.lat, lng: dummyCoord.lng, radius: radius});
+
+        const nockInstance = getSolarLayersNockInstance(dummyCoord, radius);
         nockInstance.reply(200, dummyData);
 
         return request(app)
-            .get(url)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
             .expect(200)
             .then((response) => {
                 assert.isTrue(UtilGenerator.identicalJsonStrings(response.body.solarLayers, dummyData));
             });
     });
 
-    test("whenRequestFails, then returns 500 with api error", () => {
+    test("given error on api call, when request fails, then returns 500 with api error", () => {
+        const dummyData = dummySolarLayers();
+        const dummyCoord = dummyLatLng();
+        const endpoint = getSolarLayersEndpoint({ lat: dummyCoord.lat, lng: dummyCoord.lng, radius: radius});
+
+        const apiError = new ApiError(endpoint);
+        const nockInstance = getSolarLayersNockInstance(dummyCoord, radius);
         nockInstance.replyWithError(apiError);
 
         return request(app)
-            .get(url)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
             .expect(500)
             .then((response) => {
                 assert.isTrue(UtilGenerator.identicalJsonStrings(response.body, apiError.toObject()));
@@ -98,49 +142,77 @@ describe(`GET /solar/solar-layers`, async () => {
     });
 });
 
+
+function getSolarGeotiffEndpoint(url: string) {
+    return `/solar/geotiff?${new URLSearchParams({ url: url }).toString()}`;
+}
+
+function getGeotiffNockInstance(url: string) {
+    return nock(url)
+        .get("/")
+        .query({ key: GOOGLE_KEY });
+}
+
 describe(`GET /solar/geotiff`, async () => {
-    const dummyData = dummyGeoTiff();
-    const dummyUrl = StringGenerator.generateUrl();
-    const url = `/solar/geotiff?${new URLSearchParams({ url: dummyUrl }).toString()}`;
-    const apiError = new ApiError(url);
+    test("when request is successfull, then returns 200 with geotiff", async () => {
+        const dummyData = dummyGeoTiff();
+        const dummyUrl = StringGenerator.generateUrl();
+        const endpoint = getSolarGeotiffEndpoint(dummyUrl);
 
-    const nockInstance = nock(dummyUrl).get("/").query({ key: GOOGLE_KEY });
-
-    test("whenRequestIsSucessfull, then returns 200 with geotiff", () => {
-        solar.makeGeotiff = vi.fn().mockResolvedValue(dummyData);
+        solar.makeGeotiff = vi
+            .fn()
+            .mockResolvedValue(dummyData);
+        const nockInstance = getGeotiffNockInstance(dummyUrl);
         nockInstance.reply(200, dummyData);
 
         return request(app)
-            .get(url)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
             .expect(200)
             .then((response) => {
                 assert.isTrue(UtilGenerator.identicalJsonStrings(response.body.geotiff, dummyData));
             });
     });
 
-    test("whenRequestFails, then returns 500 with api error", () => {
-        solar.makeGeotiff = vi.fn().mockResolvedValue(dummyData);
-        nockInstance.replyWithError(apiError);
+    test("when request fails, then returns 500 with api error", () => {
+        const dummyData = dummyGeoTiff();
+        const dummyUrl = StringGenerator.generateUrl();
+        const endpoint = getSolarGeotiffEndpoint(dummyUrl);
 
+        solar.makeGeotiff = vi
+            .fn()
+            .mockResolvedValue(dummyData);
+        const apiError = new ApiError(endpoint);
+        const nockInstance = getGeotiffNockInstance(dummyUrl);
+        nockInstance.replyWithError(apiError);
+        
         return request(app)
-            .get(url)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
             .expect(500)
             .then((response) => {
                 assert.isTrue(UtilGenerator.identicalJsonStrings(response.body, apiError.toObject()));
             });
     });
 
-    test("whenMakingGeotiffFails, then returns 500 with api error", () => {
+    test("when making geotiff fails, then returns 500 with api error", async () => {
+        const dummyData = dummyGeoTiff();
+        const dummyUrl = StringGenerator.generateUrl();
+        const endpoint = getSolarGeotiffEndpoint(dummyUrl);
+
         solar.makeGeotiff = vi
             .fn()
             .mockImplementation(() => {
                 throw apiError;
             })
             .mockRejectedValue({});
+        const apiError = new ApiError(endpoint);
+        const nockInstance = getGeotiffNockInstance(dummyUrl);
         nockInstance.reply(200, dummyData);
 
         return request(app)
-            .get(url)
+            .get(endpoint)
+            .set('Authorization', `Bearer ${token}`)
             .expect(500)
             .then((response) => {
                 assert.isTrue(UtilGenerator.identicalJsonStrings(response.body, apiError.toObject()));
