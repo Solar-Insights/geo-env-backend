4aa93f65fbfcd0b857b733982cc69829b067a6bb
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 20:56:41 -0400

    Mathis feat: Custom factory for testing routes (removed logging)

---
+++ tests/routes/factory.ts
@@ -0,0 +1,10 @@
+import { ServerFactory } from "@/serverFactory";
+
+export const serverFactory: ServerFactory = ServerFactory.createFactory()
+    .createApp()
+    .withDefaultMiddlewares()
+    .withAuth()
+    .withAllRouters()
+    .withResponseHandlers()
+    .withErrorMiddlewares()
+    .createServer();
