1ff859270866e91a0e1b24b7e875717807a47276
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-04-23 20:49:17 -0400

    Mathis feat: Forgot to add .env conversion for Auth Token in tests

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,5 @@ export const GOOGLE_KEY: string = process.env.GOOGLE_KEY as string;
 export const BACKEND_URL: string = process.env.BACKEND_URL as string;
 export const AUTH0_BASE_URL: string = process.env.AUTH0_BASE_URL as string;
 export const AUTH_MODE: boolean = process.env.AUTH_MODE === "true";
+export const AUTH0_TESTING_CLIENT_ID = process.env.AUTH0_TESTING_CLIENT_ID as string;
+export const AUTH0_TESTING_CLIENT_SECRET = process.env.AUTH0_TESTING_CLIENT_SECRET as string;
