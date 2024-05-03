99fe636801d2edc21c3e3acee05033c267f134e8
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-03 18:21:30 -0400

    Mathis feat: Modified userRequestLogger middleware, and created userRequest billing middleware to add every request to the database

--- src/middlewares/responseHandlers.ts
+++ src/middlewares/responseHandlers.ts
@@ -1,31 +1,52 @@
 import { Request, RequestHandler } from "express";
 import { jwtDecode } from "jwt-decode";
 import { CustomAuth0JwtPayload } from "@/services/types";
+import { getUserByAuth0IdAndEmail } from "@/db/users/operations";
+import { getTeamById } from "@/db/teams/operations";
+import { InsertRequest } from "@/db/requests/types";
+import { generateRandomUuid } from "@/db/utils";
+import { createRequest } from "@/db/requests/operations";
 
 export const userRequestLogger: RequestHandler = (req, res, next) => {
     const accessToken = getAccessTokenFromRequest(req)!;
     const decodedAccessToken: CustomAuth0JwtPayload = jwtDecode(accessToken);
+    
+    const email = decodedAccessToken.email;
+    const userId = decodedAccessToken.azp;
+    const accessPath = getAccessPathFromRequest(req);
 
-    const email = `${decodedAccessToken.email}`;
-    const accessPath = `${req.method} ${req.route.path}`;
-
+    console.log("\n--- REQUEST ---")
+    console.log(`ressource: ${accessPath}`);
     console.log(`user: ${email}`);
-    console.log(`accessing ressource: ${accessPath}\n`);
+    console.log(`user id: ${userId}`)
+    
+    next();
+};
+
+export const userRequestBilling: RequestHandler = async (req, res, next) => {
+    const accessToken = getAccessTokenFromRequest(req)!;
+    const decodedAccessToken: CustomAuth0JwtPayload = jwtDecode(accessToken);
+
+    const userId = decodedAccessToken.azp;
+    const email = decodedAccessToken.email;
+    const user = await getUserByAuth0IdAndEmail(userId, email);
+
+    const team = await getTeamById(user.team_id);
+
+    const request: InsertRequest = {
+        endpoint: getAccessPathFromRequest(req),
+        id: generateRandomUuid(),
+        team_id: team.id,
+        user_id: user.auth0_id
+    };
+    await createRequest(request);
+
+    console.log("**successfully billed to user");
 
     next();
 };
 
-export const userRequestBilling: RequestHandler = (req, res, next) => {
-    const accessToken = getAccessTokenFromRequest(req)!;
-    const decodedAccessToken: CustomAuth0JwtPayload = jwtDecode(accessToken);
-
-    const email = decodedAccessToken.email;
-    const userId = decodedAccessToken.azp;
-
-    
-};
-
-function getAccessTokenFromRequest(req: Request) {
+export function getAccessTokenFromRequest(req: Request) {
     const authorization = req.headers.authorization;
     const bearerString = "Bearer ";
     if (authorization === undefined || !authorization.startsWith(bearerString)) {
@@ -40,3 +61,7 @@ export const userResponseHandler: RequestHandler = (req, res, next) => {
     const data = res.locals.data;
     res.json(data);
 };
+
+export function getAccessPathFromRequest(req: Request) {
+    return `${req.method} ${req.route.path}`;
+}
