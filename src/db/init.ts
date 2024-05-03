72b50b0ca5a074744d2c20d3f8b60ac2d6d480db
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-03 11:30:53 -0400

    Mathis docs & feat: supabase docs and first types

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,3 +1,11 @@
+/* 
+    - Update tables definition / attributes:
+    I suggest you do it manually on the supabase table editor, then update your types using the below command. You will need to update your db calls.
+
+    - Generate types with:
+    npx supabase gen types typescript --project-id "PROJECT_ID" --schema public > ./src/db/types.ts
+*/ 
+
 import { createClient } from '@supabase/supabase-js'
 import { SUPABASE_KEY, SUPABASE_URL } from '@/config';
 
