aa1fcce2d677f4f8e68d7fa2dc9f486363ef5172
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-12 16:52:16 -0400

    Mathis chore: Added env var for Auth0 Management API support

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,8 @@ export const PORT: number = Number(process.env.PORT) || 0;
 export const GOOGLE_KEY: string = process.env.GOOGLE_KEY as string;
 export const BACKEND_URL: string = process.env.BACKEND_URL as string;
 export const AUTH0_BASE_URL: string = process.env.AUTH0_BASE_URL as string;
+export const AUTH0_CLIENT_ID: string = process.env.AUTH0_CLIENT_ID as string;
+export const AUTH0_CLIENT_SECRET: string = process.env.AUTH0_CLIENT_SECRET as string;
 export const AUTH_MODE: boolean = process.env.AUTH_MODE === "true";
 export const AUTH0_TESTING_CLIENT_ID = process.env.AUTH0_TESTING_CLIENT_ID as string;
 export const AUTH0_TESTING_CLIENT_SECRET = process.env.AUTH0_TESTING_CLIENT_SECRET as string;
