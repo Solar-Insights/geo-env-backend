3bcc1d6530e47b12be803926a0dd7b5ba95fd918
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-12 18:07:19 -0400

    Mathis feat: Functions to complete the signup. Sending email verification, password reset, and role change

--- src/api/user.ts
+++ src/api/user.ts
@@ -54,3 +54,70 @@ export async function manuallyCreateAuth0User(managementAPIToken: string, email:
             throw error;
         });
 }
+
+export async function sendEmailForEmailVerification(managementAPIToken: string, userId: string) {
+    const userData = {
+        user_id: userId
+    };
+
+    await axios({
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${managementAPIToken}`
+        },
+        responseType: "json",
+        url: `${AUTH0_BASE_URL}/api/v2/jobs/verification-email`,
+        data: userData
+    })
+        .catch((error) => {
+            console.log(error);
+            throw error;
+        });
+}
+
+export async function sendEmailForPasswordReset(email: string) {
+    const passwordResetData = {
+        client_id: AUTH0_CLIENT_ID,
+        email: email,
+        connection: 'Username-Password-Authentication'
+    };
+
+    await axios({
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        responseType: "json",
+        url: `${AUTH0_BASE_URL}/dbconnections/change_password`,
+        data: passwordResetData
+    })
+        .catch((error) => {
+            console.log(error);
+            throw error;
+        });
+}
+
+export async function assignRolesToUser(managementAPIToken: string, userId: string, roles: string[]) {
+    const rolesData = {
+        roles: roles
+    };
+
+    await axios({
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${managementAPIToken}`
+        },
+        responseType: "json",
+        url: `${AUTH0_BASE_URL}/api/v2/users/${userId}/roles`,
+        data: rolesData
+    })
+        .catch((error) => {
+            console.log(error);
+            throw error;
+        });
+}
