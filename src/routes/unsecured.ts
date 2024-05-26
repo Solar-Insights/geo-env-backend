84fb5f2a5d243d106f9de9852b2b355e45a82aff
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-26 19:04:42 -0400

    Mathis feat: unsecured request logger

--- src/routes/unsecured.ts
+++ src/routes/unsecured.ts
@@ -1,9 +1,16 @@
-import express from "express";
+import express, { Request } from "express";
 import { validateOrReject } from "class-validator";
 import { ObjectValidationError } from "@/middlewares/customErrors";
 import { NewOrganizationFormClass } from "@/dto/unsecured/newOrganizationForm";
+import { getAccessPathFromRequest } from "@/middlewares/responseHandlers";
 
 const unsecuredRouter = express.Router();
+
+function unsecuredRequestLogger(req: Request) {
+    const accessPath = getAccessPathFromRequest(req);
+    console.log(`\n--- UNSECURED REQUEST ---`);
+    console.log(`ressource: ${accessPath}`);
+}
 
 unsecuredRouter.post("/unsecured/organization", async (req, res, next) => {
     const newOrganizationFormObject = new NewOrganizationFormClass(req.body);
@@ -17,6 +24,8 @@ unsecuredRouter.post("/unsecured/organization", async (req, res, next) => {
         });
     
     // Send email to someone with info
+    unsecuredRequestLogger(req);
+
     res.status(201).json();
 });
 
