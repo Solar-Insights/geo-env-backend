0b3c399a8368d9652f7804689344469950201014
Mathis Beaudoin <mathisbeaudoin15@hotmail.com>
2024-05-29 17:21:37 -0400

    Mathis feat: userRequestBilling has been implemented with all db calls.

--- src/db/billing/operations.ts
+++ src/db/billing/operations.ts
@@ -1,13 +1,28 @@
 import { supabase } from "@/db/init";
 import { OperationValidator } from "@/db/operationValidator";
+import { MonthlyBillingField, MonthlyQuotaField } from "@/services/types";
+import { SupabaseBilling, UpdateBilling } from "./types";
 
 export async function getLatestBillingByTeamId(teamId: string) {
     const { data, error } = await supabase.from("billing")
         .select()
-        .eq("id", teamId)
+        .eq("team_id", teamId)
         .order("billing_date", { ascending: false });
 
     new OperationValidator(data, error).validateGetSingleItemRequest();
 
     return data![0];
+}
+
+export async function updateBillingById(billing: UpdateBilling, id: string) {
+    const { data, error } = await supabase.from("billing").update(billing).eq("id", id);
+
+    new OperationValidator(data, error).validateUpdateRequest();
+}
+
+export async function incrementLatestBillingField(teamId: string, billingField: MonthlyBillingField) {
+    const latestBilling: SupabaseBilling = await getLatestBillingByTeamId(teamId);
+    latestBilling[billingField] += 1;
+
+    await updateBillingById(latestBilling, latestBilling.id);
 }
--- src/middlewares/responseHandlers.ts
+++ src/middlewares/responseHandlers.ts
@@ -1,11 +1,14 @@
 import { Request, RequestHandler } from "express";
 import { jwtDecode } from "jwt-decode";
-import { CustomAuth0JwtPayload } from "@/services/types";
+import { CustomAuth0JwtPayload, MonthlyBillingField, MonthlyQuotaField, RoutesAffectingQuotas } from "@/services/types";
 import { getUserByEmail } from "@/db/users/operations";
 import { getTeamById } from "@/db/teams/operations";
 import { InsertRequest } from "@/db/requests/types";
 import { generateRandomUuid } from "@/db/utils";
 import { createRequest } from "@/db/requests/operations";
+import { getOrganizationByAccessToken } from "@/services/users";
+import { getLatestBillingByTeamId, incrementLatestBillingField } from "@/db/billing/operations";
+import { monthlyQuotaFieldToMonthlyBillingFieldMap, routeToMonthlyQuotaFieldMap } from "@/services/constants";
 
 export const userRequestLogger: RequestHandler = async (req, res, next) => {
     const decodedAccessToken: CustomAuth0JwtPayload = getDecodedAccessTokenFromRequest(req)!;
@@ -23,34 +26,22 @@ export const userRequestLogger: RequestHandler = async (req, res, next) => {
         team_id: team.id,
         user_id: user.auth0_id
     };
-    
-    await createRequest(request);
 
-    console.log("**successfully billed to user");
+    await createRequest(request);
 
     next();
 };
 
 export const userRequestBilling: RequestHandler = async (req, res, next) => {
     const decodedAccessToken: CustomAuth0JwtPayload = getDecodedAccessTokenFromRequest(req)!;
+    const organization = await getOrganizationByAccessToken(decodedAccessToken);
 
-    console.log("\n--- REQUEST ---");
-    console.log(`ressource: ${getAccessPathFromRequest(req)}`);
-    console.log(`user: ${decodedAccessToken.email}`);
-    console.log(`user id: ${decodedAccessToken.azp}`);
+    const quotaRoute = req.url as RoutesAffectingQuotas;
+    const monthlyQuotaField: MonthlyQuotaField = routeToMonthlyQuotaFieldMap[quotaRoute];
+    const monthlyBillingField: MonthlyBillingField = monthlyQuotaFieldToMonthlyBillingFieldMap[monthlyQuotaField];
+    await incrementLatestBillingField(organization.id, monthlyBillingField)
 
-    const user = await getUserByEmail(decodedAccessToken.email);
-    const team = await getTeamById(user.team_id);
-
-    const request: InsertRequest = {
-        endpoint: getAccessPathFromRequest(req),
-        id: generateRandomUuid(),
-        team_id: team.id,
-        user_id: user.auth0_id
-    };
-    await createRequest(request);
-
-    console.log("**successfully billed to user");
+    console.log("**successfully added to monthly billing");
 
     next();
 };
