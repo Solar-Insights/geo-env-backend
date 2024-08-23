5bda17596d1a417854de66c1880b1ee4c516df55
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2024-08-23 17:59:15 -0400

    fix: removed hardcoded key

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,8 +1,3 @@
-/* 
-    - Update tables definition / attributes:
-    npx supabase gen types typescript --project-id "ypapaxzpkbwjevhpokfj" --schema public > ./src/db/utils/types.ts
-*/
-
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_KEY, SUPABASE_URL } from "@/server/utils/env";
 import { Database } from "@/db/utils/types";
